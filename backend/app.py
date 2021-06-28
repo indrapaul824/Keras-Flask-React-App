3b0ef8db6af48dbc1d9a79d992ad8473ccebf038
IndraP24 <indrashis985@gmail.com>
2021-06-28 20:59:36 +0530

    Add classifier post route

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,31 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, request
+from werkzeug.wrappers import request
+from reverseProxy import proxyRequest
+from classifier import classifyImage
+
+MODE = os.getenv('FLASK_ENV')
+DEV_SERVER_URL = 'http://localhost:3000/'
 
 app = Flask(__name__)
 
+if MODE == "development":
+    app = Flask(__name__, static_folder=None)
 
 @app.route('/')
-def index():
-    return "Hello from FLASK"
+@app.route('/<path:path>')
+def index(path=''):
+    if MODE == "development":
+        return proxyRequest(DEV_SERVER_URL, path)
+    else:
+        return render_template("index.html")
+
+
+@app.route('/classify', methods=['POST'])
+def classify():
+    if (request.files['image']):
+        file = request.files['image']
+
+        res = classifyImage(file)
+        print("Model Classification: " + res)
+        return res
