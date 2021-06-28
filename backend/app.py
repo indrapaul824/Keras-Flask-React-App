d77138371ec3478749e28ba56f8bcc3a2bfecb11
IndraP24 <indrashis985@gmail.com>
2021-06-28 21:42:36 +0530

    Optimize imports

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask, render_template, request
-from werkzeug.wrappers import request
 from reverseProxy import proxyRequest
 from classifier import classifyImage
 
@@ -9,23 +8,23 @@ DEV_SERVER_URL = 'http://localhost:3000/'
 
 app = Flask(__name__)
 
+# Ignore static folder in development mode.
 if MODE == "development":
     app = Flask(__name__, static_folder=None)
 
 @app.route('/')
 @app.route('/<path:path>')
 def index(path=''):
-    if MODE == "development":
+    if MODE == 'development':
         return proxyRequest(DEV_SERVER_URL, path)
     else:
-        return render_template("index.html")
-
+        return render_template("index.html") 
 
 @app.route('/classify', methods=['POST'])
 def classify():
-    if (request.files['image']):
+    if (request.files['image']): 
         file = request.files['image']
 
-        res = classifyImage(file)
-        print("Model Classification: " + res)
-        return res
+        result = classifyImage(file)
+        print('Model classification: ' + result)        
+        return result
