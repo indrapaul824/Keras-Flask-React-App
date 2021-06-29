f9a007b9e6fac22088af1b79baf6133b66b8ed66
IndraP24 <indrashis985@gmail.com>
2021-06-29 09:46:23 +0530

    Chnaged model

--- backend/classifier.py
+++ backend/classifier.py
@@ -6,10 +6,10 @@ from werkzeug.utils import secure_filename
 from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.mobilenet import MobileNet
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
-model = MobileNet(weights='imagenet')
+model = MobileNetV2(weights='imagenet')
 
 
 def getPrediction(img_bytes, model):
